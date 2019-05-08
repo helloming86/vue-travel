d2718a2ec4c4ba0ff51210a22d1deaf91e26ed73
Nicky <helloming86@qq.com>
2019-05-08 13:44:07 +0800

    Complete HomeNav

--- src/components/HomeNav.vue
+++ src/components/HomeNav.vue
@@ -1,11 +1,15 @@
 <template>
-  <nav>
-    <div class="nav-icon" v-for="item of iconList" :key="item.id">
-      <div class="icon-img">
-        <img class="icon-img-content" :src="item.iconUrl" alt="navicon">
-      </div>
-      <p class="icon-desc">热门景点</p>
-    </div>
+  <nav class="home-nav">
+    <swiper>
+      <swiper-slide v-for="(page, index) of navPages" :key="index">
+        <div class="nav-icon" v-for="item of page" :key="item.id">
+          <div class="icon-img">
+            <img class="icon-img-content" :src="item.iconUrl" alt="navicon">
+          </div>
+          <p class="icon-desc">{{item.iconDesc}}</p>
+        </div>
+      </swiper-slide>
+    </swiper>
   </nav>
 </template>
 
@@ -16,36 +20,64 @@ export default {
     return {
       iconList: [{
         id: "0001",
-        iconUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
+        iconUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+        iconDesc: "超级长名字的热门景点1"
       },
       {
         id: "0002",
-        iconUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
+        iconUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+        iconDesc: "热门景点2"
       },
       {
         id: "0003",
-        iconUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
+        iconUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+        iconDesc: "热门景点3"
       },
       {
         id: "0004",
-        iconUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
+        iconUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+        iconDesc: "热门景点4"
       },
       {
         id: "0005",
-        iconUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
+        iconUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+        iconDesc: "热门景点5"
       },
       {
         id: "0006",
-        iconUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
+        iconUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+        iconDesc: "热门景点6"
       },
       {
         id: "0007",
-        iconUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
+        iconUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+        iconDesc: "热门景点7"
       },
       {
         id: "0008",
-        iconUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png"
+        iconUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+        iconDesc: "热门景点8"
+      },
+      {
+        id: "0009",
+        iconUrl: "http://img1.qunarzz.com/piao/fusion/1803/95/f3dd6c383aeb3b02.png",
+        iconDesc: "热门景点9"
       }]
+    }
+  },
+  computed: {
+    navPages () {
+      // 定义块级作用域常量pages
+      // js中 const 定义的块级作用于常量为对象或者数组时，可以修改
+      const pages = []
+      this.iconList.forEach((item, index) => {
+        const page = Math.floor(index / 8)
+          if(!pages[page]) {
+            pages[page] = []
+          }
+          pages[page].push(item)
+      })
+      return pages
     }
   }
 }
@@ -53,40 +85,40 @@ export default {
 
 <style lang="stylus" scoped>
   @import "~@/assets/styles/varibles.styl"
-  nav
-    width: 100%
-    overflow: hidden
+  @import "~@/assets/styles/mixins.styl"  
+  .home-nav >>> .swiper-container
     height: 0
     padding-bottom: 50%
     // background: green
-    .nav-icon
-      position: relative
-      overflow: hidden
-      float: left 
-      width: 25%
-      height: 0
-      padding-bottom: 25%
-      // background: red
-      .icon-img
-        position: absolute
-        top: 0
-        left: 0
-        right: 0
-        bottom: 1.375rem
-        box-sizing: border-box
-        padding: .313rem
-        // background: blue
-        .icon-img-content
-          display: block
-          margin: 0 auto 
-          height: 100%
-      .icon-desc
-        position: absolute
-        left: 0
-        right: 0
-        bottom: 0
-        height: 1.375rem
-        line-height: 1.375rem
-        text-align: center
-        color: $darkTextColor
+  .nav-icon
+    position: relative
+    overflow: hidden
+    float: left 
+    width: 25%
+    height: 0
+    padding-bottom: 25%
+    // background: red
+    .icon-img
+      position: absolute
+      top: 0
+      left: 0
+      right: 0
+      bottom: 1.375rem
+      box-sizing: border-box
+      padding: .313rem
+      // background: blue
+      .icon-img-content
+        display: block
+        margin: 0 auto 
+        height: 100%
+    .icon-desc
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      height: 1.375rem
+      line-height: 1.375rem
+      text-align: center
+      color: $darkTextColor
+      ellipsis()
 </style>
